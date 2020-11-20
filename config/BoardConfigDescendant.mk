c3b3962bdff7baf33c9466f992277274dc654653
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:59:36 -0300

    vendor: Force latest redfin build fingerprint
    Change-Id: I57ba4cc2443c9fba0a7233e2d942de87c8ae6a9a
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/BoardConfigDescendant.mk
+++ config/BoardConfigDescendant.mk
@@ -1,2 +1,5 @@
+# Safetynet
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+
 include vendor/descendant/config/BoardConfigKernel.mk
 include vendor/descendant/config/BoardConfigSoong.mk
