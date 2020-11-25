4d09cdca75dac0725e92757a3368b722374742d6
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-11-25 22:10:01 +0000

    More branding

--- config/branding.mk
+++ config/branding.mk
@@ -3,7 +3,7 @@ DESCENDANT_VERNUM := $(shell $(GET_VERSION) vernum)
 DESCENDANT_DISH := $(shell $(GET_VERSION) dish)
 BUILD_TYPE := $(shell $(GET_VERSION) buildtype)
 DESCENDANT_BRANDING_VERSION := $(BUILD_TYPE)
-DESCENDANT_VERSION := $(shell $(GET_VERSION) version)
+DESCENDANT_VERSION := XI
 ifeq ($(filter-out Official Mashed, $(BUILD_TYPE)),)
     PRODUCT_PACKAGES += \
         DescendantCenter
