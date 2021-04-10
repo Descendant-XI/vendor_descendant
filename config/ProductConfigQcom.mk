747c21fe1bd043a188ee850cc1804b22f06b3171
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-04-10 15:47:11 +0000

    descendant: remove duplicate data-ipa-cfg-mgr

--- config/ProductConfigQcom.mk
+++ config/ProductConfigQcom.mk
@@ -100,11 +100,6 @@ endif
 QCOM_SOONG_NAMESPACE ?= hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
 PRODUCT_SOONG_NAMESPACES += $(QCOM_SOONG_NAMESPACE)
 
-# Add data-ipa-cfg-mgr to PRODUCT_SOONG_NAMESPACES if needed
-ifneq ($(USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR),true)
-    PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr
-endif
-
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
